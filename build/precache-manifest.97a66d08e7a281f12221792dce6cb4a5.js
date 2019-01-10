3d66d8051fe693b94ec2a80cdf1a39c5e4c5893f
孙亮 <sunliang@qudian.com>
2019-01-10 17:23:05 +0800

    add:build

---
+++ build/precache-manifest.97a66d08e7a281f12221792dce6cb4a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4846613fe87af48c42a6",
+    "url": "/static/js/main.4846613f.chunk.js"
+  },
+  {
+    "revision": "4d8e8c1c2dc6712e6c41",
+    "url": "/static/js/1.4d8e8c1c.chunk.js"
+  },
+  {
+    "revision": "4846613fe87af48c42a6",
+    "url": "/static/css/main.4edeadb3.chunk.css"
+  },
+  {
+    "revision": "d0f542b66851f35f55441d748876ec62",
+    "url": "/index.html"
+  }
+];
